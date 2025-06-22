bdd5930d36dbd36f00270f35b9706f7c6b6b4fc0
AKSHAT GUPTA <akshat8036@gmail.com>
2025-06-23 05:13:15 +0530

    update

--- record_and_transcribe.sh
+++ record_and_transcribe.sh
@@ -4,6 +4,6 @@ echo " Recording idea..."
 python3 idea_recorder.py
 
 echo " Transcribing using whisper.cpp..."
-./whisper.cpp//build/bin/whisper-cli -f whisper.cpp/models/ggml-base.en.bin idea.wav
+./whisper.cpp/build/bin/whisper-cli -m ./whisper.cpp/models/ggml-base.en.bin -f idea.wav -otxt
 
 echo "Transcription saved to idea.wav.txt"
